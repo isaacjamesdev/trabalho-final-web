1e4d78d00f32e2bff5364cfec7aac08669745b19
Isac Moura Gomes <contatoisacmoura@outlook.com>
2018-12-07 00:57:15 -0300

    Fixing inconsistences

---
+++ app/api/user.js
@@ -0,0 +1,3 @@
+var mongoose = require('mongoose');
+var api = {};
+var model = mongoose.model('User');
--- app/models/product.js
+++ app/models/product.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 var schema = mongoose.Schema({
-    titulo:{
+    title:{
         type: String,
         required: true
     },

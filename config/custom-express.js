fa25ce8c73df8aa886a58e15fffd43699beaa9da
isaacjamesufc <isaacjamesufc@users.noreply.github.com>
2018-12-12 16:44:15 -0300

    cart ok

--- config/custom-express.js
+++ config/custom-express.js
@@ -10,40 +10,42 @@ var MongoStore = require('connect-mongo')(session);
 
 app.set('views', __dirname+'/../public');
 app.set('view engine', 'ejs')
+app.set('secret', 'isaacjames');
+app.set('cart',[{}]);
+
+// config session-express
+// app.use(session(
+//       {secret: 'mysupersecret',
+//       resave:'false',
+//       saveUninitalized: false,
+//       store: new MongoStore({mongooseConnection: mongoose.connection}),
+//       cookie:{maxAge: 100 * 60 * 1000}
+// }));
+// app.get('/',(req,res)=>{
+//       req.session.user = token;
+// })
 
 app.use(bodyParser.urlencoded({
       extended:true
-  }));
-
-// config session-express
-app.use(session(
-      {secret: 'mysupersecret',
-      resave:'false',
-      saveUninitalized: false,
-      store: new MongoStore({mongooseConnection: mongoose.connection}),
-      cookie:{maxAge: 100 * 60 * 1000}
 }));
+app.use( express.static('./public'))
 
-app.set('secret', 'isaacjames');
-// login-user
+app.use((req,res,next)=>{
+      res.locals.cart = app.get('cart');
+      next();
+});
 
 app.use((req,res,next)=>{
       res.locals.user = app.get('name');
       res.locals.id = app.get('id');
       next();
 });
-
-
-app.use( express.static('./public'))
 // auth precisa ser carregado primeiro
 consign({cwd:'app'})
-      .include('models')
-      .then('api')
-      .then('routes/auth.js')
-      .then('routes')
+            .include('models')
+            .then('api')
+            .then('routes/auth.js')
+            .then('routes')
       .into(app);
-app.get('/',(req,res)=>{
-      req.session.user = token;
-})
 
 module.exports = app;
